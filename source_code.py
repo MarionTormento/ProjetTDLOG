fb0c09fb8ebe766eb0d41689ef2f0411c56e2a6a
MarionTormento <marion.tormento@eleves.enpc.fr>
2015-12-16 13:26:25 +0100

    collecte des mots, les attache à un fichier avec saut de ligne, les store dans un tableau

--- source_code.py
+++ source_code.py
@@ -24,8 +24,8 @@ class fch_interro():
 		self.fiche = open(self.name, "w")  # ou "a"? ou créer un conflit si le nom est déjà utilisé 
 
 	def collect_data(self, word_lg1, word_lg2): #remplit le fichier la première fois
-		definition = word_lg1 + ":" + word_lg2
-		self.fiche = open(self.name, "w")
+		definition = word_lg1 + ":" + word_lg2 + '\n'
+		self.fiche = open(self.name, "a")
 		self.fiche.write(definition)
 
 	def read_file(self): #collecte ligne par ligne une premère fois
@@ -33,15 +33,18 @@ class fch_interro():
 			for line in data:
 				self.tableau.append(line)
 				self.nb_words += 1
+		for i in range(f.nb_words):
+			print(self.tableau[i])
+
 
 	def file_to_tableau(self): #convertit le fichier texte en un tableau ou on trouve en indice par les mots langue 1 et en pair les mots langue 2
 		self.read_file()
-		words = [0 for i in 2*self.nb_words]
 		for i in range(self.nb_words):
 			word_lg1 = self.tableau[i].split(':', 1)[0] #mot dans la langue 1
-			word_lg2 = self.tableau[i].split(':', 1)[1] #mot correspondant dans la langue 2
-			self.tableau[i] = world_lg1 
-			self.tableau.append(world_lg2) #(prend la position nb_words+i)
+			word_intermediaire = self.tableau[i].split(':', 1)[1] #mot correspondant dans la langue 2 + saut de ligne
+			word_lg2 = word_intermediaire.split('\n', 1)[0] #mot correspondant dans la langue 2
+			self.tableau[i] = word_lg1 
+			self.tableau.append(word_lg2) #(prend la position nb_words+i)
 
 	#def choose_lg(self): #renvoi 1 si premier langage, 2 si deuxième
 
@@ -53,7 +56,12 @@ class fch_interro():
 
 f = fch_interro()
 f.collect_data("bonjour", "hello")
-
+f.collect_data("bonjour2", "hello2")
+f.collect_data("bonjour3", "hello3")
+f.collect_data("bonjour4", "hello4")
+f.file_to_tableau()
+for i in range(2*f.nb_words):
+	print(f.tableau[i])
 
 
 
