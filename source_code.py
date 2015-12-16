33b719404159838f7e2d40b06e2384980e4ffad7
MarionTormento <marion.tormento@eleves.enpc.fr>
2015-12-16 13:10:49 +0100

    Commit Marion, code fonctionnel pour l'ajout de mot dans un fichier à tester sur l'interface graphique

--- source_code.py
+++ source_code.py
@@ -3,6 +3,10 @@
 # créer un fichier directement dès que les gens dépose leurs informations 
 # anna faut pusher systmatiquement et t'assurer que c'est bien passé !
 # choix de la langue d'interrogation
+# importation modulaire du code dans interface
+# distance de Levenshtein
+# créer une fonction choix du nom dans l'interface
+
 
 import random
 
@@ -10,48 +14,46 @@ import random
 # Fiche de vocabulaire sur un thème unique, choix du titre du fichier, collect data à partir de l'interface graphique
 class fch_interro():
 	def __init__(self):
-		self.fiche = create_file(self)
 		self.nb_words = 0
-		self.name = ""
+		self.name = "test.txt"
 		self.tableau = []
+		self.fiche = self.create_file()
 
 	def create_file(self): #crée un fichier .txt qui restera en mémoire à terme
 		# self.name = ask for name + .txt
 		self.fiche = open(self.name, "w")  # ou "a"? ou créer un conflit si le nom est déjà utilisé 
 
-	def collect_data(word_lg1,word_lg2): #remplit le fichier la première fois
+	def collect_data(self, word_lg1, word_lg2): #remplit le fichier la première fois
 		definition = word_lg1 + ":" + word_lg2
 		self.fiche = open(self.name, "w")
-		self.fiche.append(definition)
+		self.fiche.write(definition)
 
-    def read_file(self): # collecte ligne par ligne une premère fosi 
-        with open(self.name, "r") as data:
-            for line in data:
-                self.tableau.append(line)
-                self.nb_words += 1
+	def read_file(self): #collecte ligne par ligne une premère fois
+		with open(self.name, "r") as data:
+			for line in data:
+				self.tableau.append(line)
+				self.nb_words += 1
 
-    def file_to_tableau(self):
-    	self.read_file()
-        for i in range(self.nb_words):
-            indice_imp = int(float(self.tableau_idx[i].split(':', 1)[0]))
-            info = self.tableau_idx[i].split(' ', 1)[1]
-            self.tableau_idx[i] = indice_imp
-
-	def choose_lg(self): #renvoi 1 si premier langage, 2 si deuxième
-
-    def collect_info(self):
-        for i in range(self.nb_total_elements):
-            indice_imp = int(float(self.tableau_idx[i].split(':', 1)[0]))
-            info = self.tableau_idx[i].split(' ', 1)[1]
-            self.tableau_idx[i] = indice_imp
-            self.tableau_info.append(info)
-
-
-	def interrogate(self):
-		idx_language = self.choose_lg()
-		vocab = [i for i in range(self.nb_words)]
+	def file_to_tableau(self): #convertit le fichier texte en un tableau ou on trouve en indice par les mots langue 1 et en pair les mots langue 2
+		self.read_file()
+		words = [0 for i in 2*self.nb_words]
 		for i in range(self.nb_words):
-			idx_rdm = random.choice(vocab)
+			word_lg1 = self.tableau[i].split(':', 1)[0] #mot dans la langue 1
+			word_lg2 = self.tableau[i].split(':', 1)[1] #mot correspondant dans la langue 2
+			self.tableau[i] = world_lg1 
+			self.tableau.append(world_lg2) #(prend la position nb_words+i)
+
+	#def choose_lg(self): #renvoi 1 si premier langage, 2 si deuxième
+
+#	def interrogate(self):
+#		idx_language = self.choose_lg()
+#		vocab = [i for i in range(self.nb_words)]
+#		for i in range(self.nb_words):
+#			idx_rdm = random.choice(vocab)
+
+f = fch_interro()
+f.collect_data("bonjour", "hello")
+
 
 
 
