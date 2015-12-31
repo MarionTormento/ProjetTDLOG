f61e9155801d5c5501aad7a79bf7fc98f31fa8b6
Anna Le Cerf <Anna@pc39.home>
2015-12-31 10:50:19 +0100

    Utilisation de QLineEdit et d'une classe secondaire pour Préparation#

--- Projet.py
+++ Projet.py
@@ -1,46 +1,78 @@
 import sys
 from PyQt4 import QtGui
 from PyQt4 import QtCore
+from PyQt4.QtCore import SIGNAL
 
 from source_code import *
 
-class InterfaceGraphique(QtGui.QWidget):
+class Preparation(QtGui.QWidget):
+
+	def __init__(self,parent=None):
+		super(Preparation,self).__init__(parent)
+		self.setWindowTitle("Preparation")
+		'''Champs pour rentrer les mots'''
+		self.line1=QtGui.QLineEdit(self)
+		self.line2=QtGui.QLineEdit(self)
+		'''Bouton pour passer à une définition suivante'''
+		self.bouton=QtGui.QPushButton("Définition suivante",self)
+		self.bouton.clicked.connect(self.ok_m)
+		'''Bouton pour terminer la préparation'''
+		self.terminer=QtGui.QPushButton("Préparation terminée",self)
+		self.terminer.clicked.connect(self.fermer)
+		'''On place le tout dans la fenêtre'''
+		posit=QtGui.QGridLayout()
+		posit.addWidget(self.line1,0,0)
+		posit.addWidget(self.line2,1,0)
+		posit.addWidget(self.bouton,2,0)
+		posit.addWidget(self.terminer,3,0)
+		self.setLayout(posit)
+		'''Liste pour mise en mémoire'''
+		self.french=[]
+		self.english=[]
+
+	def ok_m(self):
+		'''Récupération des valeurs'''
+		fr=self.line1.text()
+		eng=self.line2.text()
+		self.french.append(fr)
+		self.english.append(eng)
+		print(self.french)
+		print(self.english)
+		self.close()
+
+	def fermer(self):
+		self.close
+
+class InterfaceGraphique(QtGui.QMainWindow):
 	
-	def __init__(self):
-		super(InterfaceGraphique,self).__init__()
-		self.initUI()
-	
-	def initUI(self):
+	def __init__(self,parent=None):
+		super(InterfaceGraphique,self).__init__(parent)
+		
+		self.setGeometry(300,300,290,150)
+		self.setWindowTitle("Aide à la révision")
+		'''Bouton préparation'''
 		self.boutonPreparation=QtGui.QPushButton("Préparation",self)#création de l'onglet de préparation
 		self.boutonPreparation.move(20,20)
 		self.boutonPreparation.clicked.connect(self.preparer)#appel de la fonction si on clique sur l'onglet
-		
+		'''Bouton évaluation'''
 		self.boutonEvaluation=QtGui.QPushButton("Evaluation",self)#création de l'onglet d'évaluation
 		self.boutonEvaluation.move(150,20)
 		self.boutonEvaluation.clicked.connect(self.evaluer)#appel de la fonction si on clique sur cet onglet
 
-		self.liste_francais=[]
-		self.liste_anglais=[]
-
-		self.setGeometry(300,300,290,150)
-		self.setWindowTitle("Aide à la révision")
-		self.show()
 	
 	def preparer(self):
-		francais, ok1=QtGui.QInputDialog.getText(self,"Préparation","Mot français:")#on rentre le mot français
-		anglais, ok2=QtGui.QInputDialog.getText(self,"Préparation","Mot anglais:")#on rentre la traduction
-		if ok1 and ok2:
-			self.liste_francais.append(francais)#mise en mémoire
-			self.liste_anglais.append(anglais)#mise en mémoire
-			print(self.liste_francais)#affiche (mot)
-			print(self.liste_anglais)#affiche (mot)
+		'''On fait appel à la classe préparation'''
+		self.prepa=Preparation()
+		self.prepa.show()
 
 	def evaluer(self):
-		dialogue=QtGui.QInputDialog.getText(self,"Evaluation","Mot français:")
+		self.prepa=Preparation()
+		self.prepa.show()
 
 def main():
 	app=QtGui.QApplication(sys.argv)
 	interface=InterfaceGraphique()
+	interface.show()
 	sys.exit(app.exec_())
 
 if __name__=='__main__':
