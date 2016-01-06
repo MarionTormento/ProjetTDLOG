6e6792658a78d7886c923b1b61f33553670abcd7
MarionTormento <marion.tormento@eleves.enpc.fr>
2016-01-06 10:41:02 +0100

    ouverture de la fenetre nom, création du fichier, puis ouverture de la fenetre préparation, mais on écrit pas encore les mots

--- code_anna+marion.py
+++ code_anna+marion.py
@@ -9,7 +9,8 @@ class Preparation(QtGui.QWidget):
 
 	def __init__(self, fch_interro):
 		super(Preparation,self).__init__()
-		self.setWindowTitle("Preparation de la fiche self.name")
+		self.f = fch_interro
+		self.setWindowTitle("Preparation de la fiche " + fch_interro.name)
 		'''Champs pour rentrer les mots'''
 		self.line1=QtGui.QLineEdit(self)
 		self.line2=QtGui.QLineEdit(self)
@@ -20,22 +21,21 @@ class Preparation(QtGui.QWidget):
 		self.terminer=QtGui.QPushButton("Préparation terminée",self)
 		self.terminer.clicked.connect(self.fermer)
 		'''On place le tout dans la fenêtre'''
-		posit=QtGui.QGridLayout()
+		posit = QtGui.QGridLayout()
 		posit.addWidget(self.line1,0,0)
 		posit.addWidget(self.line2,1,0)
 		posit.addWidget(self.bouton,2,0)
 		posit.addWidget(self.terminer,3,0)
 		self.setLayout(posit)
 
+
 	def ok_m(self):
 		'''Récupération des valeurs'''
-		fr=self.line1.text()
-		eng=self.line2.text()
-		self.french.append(fr)
-		self.english.append(eng)
-		print(self.french)
-		print(self.english)
-		self.close()
+		word_lg1 = self.line1.text()
+		word_lg2 = self.line2.text()
+		print(word_lg1, word_lg2)
+		print(self.f.nametxt)
+		self.f.collect_data(word_lg1, word_lg2)
 
 	def fermer(self):
 		self.close
@@ -45,6 +45,7 @@ class window_name(QtGui.QWidget):
 
 	def __init__(self, fch_interro):
 		super(window_name, self).__init__()
+		self.open = True
 		self.f = fch_interro()
 		self.setWindowTitle("Nommer votre fiche de révsion")
 		'''Champs pour rentrer les mots'''
@@ -57,12 +58,18 @@ class window_name(QtGui.QWidget):
 		posit.addWidget(self.line_name,0,0)
 		posit.addWidget(self.bouton_name,2,0)
 		self.setLayout(posit)
-
+		
 	def ok_name(self):
 		'''Récupération du nom'''
 		self.f.name = self.line_name.text()
 		self.f.create_file()
-		self.close()			
+		self.close()		
+		self.prepa_fiche = Preparation(self.f)
+		self.prepa_fiche.show()
+
+	def next_wd(self):
+		self.open = False
+					
 
 
 class InterfaceGraphique(QtGui.QMainWindow):
@@ -74,19 +81,21 @@ class InterfaceGraphique(QtGui.QMainWindow):
 		'''Bouton préparation'''
 		self.boutonPreparation=QtGui.QPushButton("Préparation",self)#création de l'onglet de préparation
 		self.boutonPreparation.move(20,20)
-		self.boutonPreparation.clicked.connect(self.preparer)#appel de la fonction si on clique sur l'onglet
+		self.boutonPreparation.clicked.connect(self.nommer_fichier)#appel de la fonction si on clique sur l'onglet
 		'''Bouton évaluation'''
 		self.boutonEvaluation=QtGui.QPushButton("Evaluation",self)#création de l'onglet d'évaluation
 		self.boutonEvaluation.move(150,20)
 		self.boutonEvaluation.clicked.connect(self.evaluer)#appel de la fonction si on clique sur cet onglet
 
 	
-	def preparer(self):
+	def nommer_fichier(self):
 		'''On fait appel à la classe préparation'''
 		self.fiche = window_name(fch_interro)
 		self.fiche.show()
-		self.prepa = Preparation(self.fiche.f)
-		self.prepa.show()
+#		print(self.fiche.open)
+#		if self.fiche.open == False:
+#			self.prepa = Preparation(self.fiche.f)
+#			self.prepa.show()
 
 
 	def evaluer(self):
